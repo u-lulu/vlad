cabfb61384e67967714769c0b28071924904271b
ululu <mtmerlino@gmail.com>
2023-05-16 15:03:31 -0400

    refactoring of output

--- vlad.py
+++ vlad.py
@@ -28,7 +28,7 @@ def list_of_d6(count):
 		out.append(d6())
 	return out
 
-def debug(s, channel):
+def tracelog(s, channel):
 	print(s)
 	details_string[channel] += s + "\n"
 
@@ -86,77 +86,77 @@ async def combat_drill(ctx,
 	energy = list_of_d6(2 if crit else 1)
 	bonus = list_of_d6(bonus_dice * (2 if crit else 1))
 	
-	debug("--- Initial dice results ---", channel_id)
-	debug(f"Kinetic: {kinetic}", channel_id)
-	debug(f"Energy: {energy}", channel_id)
-	debug(f"Bonus: {bonus}", channel_id)
+	tracelog("--- Initial dice results ---", channel_id)
+	tracelog(f"Kinetic: {kinetic}", channel_id)
+	tracelog(f"Energy: {energy}", channel_id)
+	tracelog(f"Bonus: {bonus}", channel_id)
 	
 	heat = 0
 	
 	while 1 in kinetic:
-		debug("--- Kinetic reroll loop ---", channel_id)
+		tracelog("--- Kinetic reroll loop ---", channel_id)
 		for i in range(len(kinetic)):
 			if kinetic[i] == 1:
-				debug(f"Rerolled result in position {i} (+1 heat)", channel_id)
+				tracelog(f"Rerolled result in position {i} (+1 heat)", channel_id)
 				kinetic[i] = d6()
 				heat += 1
 				if target_has_status:
-					debug(f"Added bonus d6", channel_id)
+					tracelog(f"Added bonus d6", channel_id)
 					bonus.append(d6())
 					if crit:
-						debug(f"Added bonus d6", channel_id)
+						tracelog(f"Added bonus d6", channel_id)
 						bonus.append(d6())
-		debug(f"Reroll pass results: {kinetic}", channel_id)
+		tracelog(f"Reroll pass results: {kinetic}", channel_id)
 	
-	debug("--- END OF KINETIC DAMAGE ---", channel_id)
-	debug(f"Final results: {kinetic}", channel_id)
-	debug(f"Current Bonus dice: {bonus}", channel_id)
+	tracelog("--- END OF KINETIC DAMAGE ---", channel_id)
+	tracelog(f"Final results: {kinetic}", channel_id)
+	tracelog(f"Current Bonus dice: {bonus}", channel_id)
 	
 	while 1 in energy:
-		debug("--- Energy reroll loop ---", channel_id)
+		tracelog("--- Energy reroll loop ---", channel_id)
 		for i in range(len(energy)):
 			if energy[i] == 1:
-				debug(f"Rerolled result in position {i} (+1 heat)", channel_id)
+				tracelog(f"Rerolled result in position {i} (+1 heat)", channel_id)
 				energy[i] = d6()
 				heat += 1
 				if target_has_status:
-					debug(f"Added bonus d6", channel_id)
+					tracelog(f"Added bonus d6", channel_id)
 					bonus.append(d6())
 					if crit:
-						debug(f"Added bonus d6", channel_id)
+						tracelog(f"Added bonus d6", channel_id)
 						bonus.append(d6())
-		debug(f"Reroll pass results: {energy}", channel_id)
+		tracelog(f"Reroll pass results: {energy}", channel_id)
 
-	debug("--- END OF ENERGY DAMAGE ---", channel_id)
-	debug(f"Final results: {energy}", channel_id)
-	debug(f"Current Bonus dice: {bonus}", channel_id)
+	tracelog("--- END OF ENERGY DAMAGE ---", channel_id)
+	tracelog(f"Final results: {energy}", channel_id)
+	tracelog(f"Current Bonus dice: {bonus}", channel_id)
 	
 	while 1 in bonus:
-		debug("--- Bonus reroll loop ---", channel_id)
+		tracelog("--- Bonus reroll loop ---", channel_id)
 		for i in range(len(bonus)):
 			if bonus[i] == 1:
-				debug(f"Rerolled result in position {i} (+1 heat)", channel_id)
+				tracelog(f"Rerolled result in position {i} (+1 heat)", channel_id)
 				bonus[i] = d6()
 				heat += 1
 				if target_has_status:
-					debug(f"Added bonus d6", channel_id)
+					tracelog(f"Added bonus d6", channel_id)
 					bonus.append(d6())
 					if crit:
-						debug(f"Added bonus d6", channel_id)
+						tracelog(f"Added bonus d6", channel_id)
 						bonus.append(d6())
-		debug(f"Reroll pass results: {bonus}", channel_id)
+		tracelog(f"Reroll pass results: {bonus}", channel_id)
 	
-	debug("--- END OF BONUS DAMAGE ---", channel_id)
-	debug(f"Final results: {bonus}", channel_id)
+	tracelog("--- END OF BONUS DAMAGE ---", channel_id)
+	tracelog(f"Final results: {bonus}", channel_id)
 	
 	kinetic.sort()
 	energy.sort()
 	bonus.sort()
 	
-	debug("--- Sorting ---", channel_id)
-	debug(f"Kinetic: {kinetic}", channel_id)
-	debug(f"Energy: {energy}", channel_id)
-	debug(f"Bonus: {bonus}", channel_id)
+	tracelog("--- Sorting ---", channel_id)
+	tracelog(f"Kinetic: {kinetic}", channel_id)
+	tracelog(f"Energy: {energy}", channel_id)
+	tracelog(f"Bonus: {bonus}", channel_id)
 	
 	if crit:
 		lower_bound_kinetic = int(len(kinetic)/2)
@@ -167,10 +167,10 @@ async def combat_drill(ctx,
 		energy = energy[lower_bound_energy:]
 		bonus = bonus[lower_bound_bonus:]
 		
-		debug("--- After crit split ---", channel_id)
-		debug(f"Kinetic: {kinetic}", channel_id)
-		debug(f"Energy: {energy}", channel_id)
-		debug(f"Bonus: {bonus}", channel_id)
+		tracelog("--- After crit split ---", channel_id)
+		tracelog(f"Kinetic: {kinetic}", channel_id)
+		tracelog(f"Energy: {energy}", channel_id)
+		tracelog(f"Bonus: {bonus}", channel_id)
 	
 	kinetic_total = sum(kinetic)
 	energy_total = sum(energy)
@@ -178,14 +178,28 @@ async def combat_drill(ctx,
 	
 	full_total = kinetic_total + energy_total + bonus_total
 	
-	debug("--- Totals ---", channel_id)
-	debug(f"Kinetic damage: {kinetic_total}", channel_id)
-	debug(f"Energy damage: {energy_total}", channel_id)
-	debug(f"Bonus damage: {bonus_total}", channel_id)
-	debug(f"Heat generated: {heat}", channel_id)
-	debug(f"Full damage: {full_total}", channel_id)
+	tracelog("--- Totals ---", channel_id)
+	tracelog(f"Kinetic damage: {kinetic_total}", channel_id)
+	tracelog(f"Energy damage: {energy_total}", channel_id)
+	tracelog(f"Bonus damage: {bonus_total}", channel_id)
+	tracelog(f"Heat generated: {heat}", channel_id)
+	tracelog(f"Full damage: {full_total}", channel_id)
 	
-	message = f"**__Combat Drill__**\n🎯 Crit: **{yn(crit)}**\n💫 Status Effect: **{yn(target_has_status)}**\n🎲 Initial Bonus: **{bonus_dice}d6 + {bonus_flat}**\n\n⚔️ Kinetic: **{str(kinetic_total)}** `{str(kinetic)}`\n⚡ Energy: **{str(energy_total)}** `{str(energy)}`\n🎲 Bonus: **{str(bonus_total)}** `{str(bonus)}`\n🔥 Heat taken: **{str(heat)}**\n\n**Total damage: {str(full_total)}**"
+	#message = f"**__Combat Drill__**\n🎯 Crit: **{yn(crit)}**\n💫 Status Effect: **{yn(target_has_status)}**\n{f'🎲 Initial Bonus: **{bonus_dice}d6 + {bonus_flat}**' if bonus_dice > 0 or bonus_flat > 0 else ''}\n\n⚔️ Kinetic: **{str(kinetic_total)}** `{str(kinetic)}`\n⚡ Energy: **{str(energy_total)}** `{str(energy)}`\n🎲 Bonus: **{str(bonus_total)}** `{str(bonus)}`\n🔥 Heat taken: **{str(heat)}**\n\n**Total damage: {str(full_total)}**"
+	
+	message = "**__Combat Drill__**"
+	message += f"\n🎯 Crit: **{yn(crit)}**"
+	message += f"\n💫 Status Effect: **{yn(target_has_status)}**"
+	
+	if bonus_dice > 0 and bonus_flat > 0:
+		message += f"\n🎲 Initial Bonus: **{bonus_dice}d6 + {bonus_flat}**"
+	elif bonus_dice > 0:
+		message += f"\n🎲 Initial Bonus: **{bonus_dice}d6**"
+	elif bonus_flat > 0:
+		message += f"\n🎲 Initial Bonus: **{bonus_flat}** (flat)"
+	
+	message += f"\n\n⚔️ Kinetic: **{str(kinetic_total)}** `{str(kinetic)}`\n⚡ Energy: **{str(energy_total)}** `{str(energy)}`\n🎲 Bonus: **{str(bonus_total)}** `{str(bonus)}`\n🔥 Heat taken: **{str(heat)}**\n\n**Total damage: {str(full_total)}**"
+	
 	sent_message = await ctx.respond(message)
 	sent_message = await sent_message.original_response()
 	
